fd846e5645b21d7f5b71c9f3567b24a50bb468c9
Alois <alois.garnier@hotmail.com>
2023-03-31 15:46:59 +0200

    Initial commit

---
+++ src/main/java/fr/eql/ai113/mille/arts/back/MilleArtsBackApplication.java
@@ -0,0 +1,13 @@
+package fr.eql.ai113.mille.arts.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilleArtsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilleArtsBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eql/ai113/mille/arts/back/MilleArtsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eql.ai113.mille.arts.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilleArtsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
