f27dbd3b12055d79646fdb48b9e8e83ee592947a
Alois <alois.garnier@hotmail.com>
2023-05-26 11:29:10 +0200

    SQL Import improvement

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,26 +8,39 @@ INSERT INTO `address` (`id`, `name`, `street`, `street_number`, `customer_id`, `
 INSERT INTO `address` (`id`, `name`, `street`, `street_number`, `customer_id`, `city_id`) VALUES (2, 'Maison', 'rue des Bleuets', '3bis', 2, 1);
 INSERT INTO `address` (`id`, `name`, `street`, `street_number`, `customer_id`, `city_id`) VALUES (3, 'Chez Mamie', 'rue des Lilas', '127', 2, 2);
 
-INSERT INTO `decoration` (`id`, `name`, `picture`, `description`) VALUES (1, 'Chauve-souris', 'https://i.pinimg.com/564x/3e/1e/07/3e1e07ef7dfe787049fff063d7b12684.jpg', 'Description 1');
-INSERT INTO `decoration` (`id`, `name`, `picture`, `description`) VALUES (2, 'Chauve-souris 2', 'https://i.pinimg.com/564x/3e/1e/07/3e1e07ef7dfe787049fff063d7b12684.jpg', 'Description 2');
+INSERT INTO `decoration` (`id`, `name`, `picture`, `description`) VALUES (1, 'Maison enneigée', 'https://i.pinimg.com/750x/d4/ce/52/d4ce5208d8b912a89ea66c0f3f34c2ba.jpg', 'Maisonnette enneigée et décorée, d''environ 20 cm de haut, se fondant parfaitement dans un décor de Noël');
+INSERT INTO `decoration` (`id`, `name`, `picture`, `description`) VALUES (2, 'Sac en tissu', 'https://i.pinimg.com/750x/a2/8b/c1/a28bc1d38f92160937ba6296f435ca36.jpg', 'Joli sac en tissu accompagné de 3 sucres d''orge décoratifs');
+INSERT INTO `decoration` (`id`, `name`, `picture`, `description`) VALUES (3, 'Champignon de Noël', 'https://i.pinimg.com/750x/42/72/9c/42729ce3a61105c4a75486f850206f75.jpg', 'Champignon de Noël vendu à l''unité');
+INSERT INTO `decoration` (`id`, `name`, `picture`, `description`) VALUES (4, 'Sac en tissu', 'https://i.pinimg.com/750x/74/46/d6/7446d611844ad153a50f267d06006389.jpg', 'Petit sac en tissu vendu à l''unité');
+INSERT INTO `decoration` (`id`, `name`, `picture`, `description`) VALUES (5, 'Bonhomme de neige', 'https://i.pinimg.com/750x/7c/a6/e7/7ca6e70767cde4b87ed6fc516995dab4.jpg', 'Magnifique bonhomme de neige d''environ 10 cm de haut');
 
-INSERT INTO `tag` (`id`, `name`) VALUES (1, 'Halloween');
-INSERT INTO `tag` (`id`, `name`) VALUES (2, 'Violet');
-INSERT INTO `tag` (`id`, `name`) VALUES (3, 'Rouge');
+INSERT INTO `tag` (`id`, `name`) VALUES (1, 'Noël');
+INSERT INTO `tag` (`id`, `name`) VALUES (2, 'Maison');
+INSERT INTO `tag` (`id`, `name`) VALUES (3, 'Sac');
+INSERT INTO `tag` (`id`, `name`) VALUES (4, 'Champignon');
+INSERT INTO `tag` (`id`, `name`) VALUES (5, 'Bonhomme de neige');
 
 INSERT INTO `decoration_tag` (`id`, `decoration_id`, `tag_id`) VALUES (1, 1, 1);
 INSERT INTO `decoration_tag` (`id`, `decoration_id`, `tag_id`) VALUES (2, 2, 1);
-INSERT INTO `decoration_tag` (`id`, `decoration_id`, `tag_id`) VALUES (3, 1, 2);
-INSERT INTO `decoration_tag` (`id`, `decoration_id`, `tag_id`) VALUES (4, 2, 3);
+INSERT INTO `decoration_tag` (`id`, `decoration_id`, `tag_id`) VALUES (3, 3, 1);
+INSERT INTO `decoration_tag` (`id`, `decoration_id`, `tag_id`) VALUES (4, 4, 1);
+INSERT INTO `decoration_tag` (`id`, `decoration_id`, `tag_id`) VALUES (5, 5, 1);
+INSERT INTO `decoration_tag` (`id`, `decoration_id`, `tag_id`) VALUES (6, 1, 2);
+INSERT INTO `decoration_tag` (`id`, `decoration_id`, `tag_id`) VALUES (7, 2, 3);
+INSERT INTO `decoration_tag` (`id`, `decoration_id`, `tag_id`) VALUES (8, 3, 4);
+INSERT INTO `decoration_tag` (`id`, `decoration_id`, `tag_id`) VALUES (9, 4, 3);
+INSERT INTO `decoration_tag` (`id`, `decoration_id`, `tag_id`) VALUES (10, 5, 5);
 
-INSERT INTO `price` (`id`, `amount`) VALUES (1, 10);
-INSERT INTO `price` (`id`, `amount`) VALUES (2, 11);
-INSERT INTO `price` (`id`, `amount`) VALUES (3, 12);
+INSERT INTO `price` (`id`, `amount`) VALUES (1, 20);
+INSERT INTO `price` (`id`, `amount`) VALUES (2, 15);
+INSERT INTO `price` (`id`, `amount`) VALUES (3, 12.5);
+INSERT INTO `price` (`id`, `amount`) VALUES (4, 10);
 
-INSERT INTO `decoration_price` (`id`, `decoration_id`, `price_id`, `addition_date`, `withdrawal_date`) VALUES (1, 1, 1, '2017-11-16', '2017-11-16');
-INSERT INTO `decoration_price` (`id`, `decoration_id`, `price_id`, `addition_date`, `withdrawal_date`) VALUES (2, 2, 1, '2017-11-16', '2017-11-16');
-INSERT INTO `decoration_price` (`id`, `decoration_id`, `price_id`, `addition_date`, `withdrawal_date`) VALUES (3, 1, 2, '2017-11-16', null);
-INSERT INTO `decoration_price` (`id`, `decoration_id`, `price_id`, `addition_date`, `withdrawal_date`) VALUES (4, 2, 3, '2017-11-16', null);
+INSERT INTO `decoration_price` (`id`, `decoration_id`, `price_id`, `addition_date`, `withdrawal_date`) VALUES (1, 1, 1, '2023-05-26', null);
+INSERT INTO `decoration_price` (`id`, `decoration_id`, `price_id`, `addition_date`, `withdrawal_date`) VALUES (2, 2, 2, '2023-05-26', null);
+INSERT INTO `decoration_price` (`id`, `decoration_id`, `price_id`, `addition_date`, `withdrawal_date`) VALUES (3, 3, 3, '2023-05-26', null);
+INSERT INTO `decoration_price` (`id`, `decoration_id`, `price_id`, `addition_date`, `withdrawal_date`) VALUES (4, 4, 4, '2023-05-26', null);
+INSERT INTO `decoration_price` (`id`, `decoration_id`, `price_id`, `addition_date`, `withdrawal_date`) VALUES (5, 5, 2, '2023-05-26', null);
 
 INSERT INTO `command` (`id`, `customer_id`, `order_date`) VALUES (1, 2, '2023-03-12');
 INSERT INTO `command` (`id`, `customer_id`, `order_date`) VALUES (2, 2, '2023-01-02');
